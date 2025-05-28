4bd9289c96c114d0b2c48f6647b04fcc839574cc
lijiebuliao <112558835+lijiebuliao@users.noreply.github.com>
2025-05-28 15:28:57 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import createPlugins from './plugins'
+import createPlugins from './vite/plugins'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: createPlugins(),
